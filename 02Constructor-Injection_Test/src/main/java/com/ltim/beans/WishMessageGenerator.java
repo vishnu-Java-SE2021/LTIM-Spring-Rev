20ddbbf732ede70cae893f8d1f7e8d795588603b
LENOVO <LENOVO@DESKTOP-4QSLTLE>
2023-02-06 19:02:51 +0530

    Constructor Injection Test

---
+++ 02Constructor-Injection_Test/src/main/java/com/ltim/beans/WishMessageGenerator.java
@@ -0,0 +1,28 @@
+package com.ltim.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+
+	private Date date;
+		
+	public WishMessageGenerator(Date date) {
+		this.date=date;
+	}
+
+	public String getMessage(String user) {
+		
+		int hour = date.getHours();
+				
+		if(hour<12)
+			return "Good Morning "+user;
+		else if(hour<16)
+			return "Good Afternoon "+user;
+		else if(hour<20)
+			return "Good Evening "+user;
+		else
+			return "Good Night "+user;
+	}
+	
+	
+}
